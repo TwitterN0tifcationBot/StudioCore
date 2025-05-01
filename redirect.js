c14fa2c30046333d3e2e86ff434bb0cc8d773743
Coder.Vibez <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2025-05-01 22:08:04 +0200

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,11 +1,1 @@
-// redirect.js
 
-// Check if the user is on a device (mobile, tablet, etc.)
-function isDevice() {
-    return /Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
-}
-
-// Redirect to shutdown.html if the user is on a device
-if (isDevice()) {
-    window.location.href = "shutdown.html";
-}
