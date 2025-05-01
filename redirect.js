95c65072d837c2025885ef78c96de1098a709be3
Coder.Vibez <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2025-05-01 21:04:16 +0200

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -1,2 +1,11 @@
-if windows:connect(true)
-windows.location="shutdown.html"
+// redirect.js
+
+// Check if the user is on a device (mobile, tablet, etc.)
+function isDevice() {
+    return /Mobi|Android|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
+}
+
+// Redirect to shutdown.html if the user is on a device
+if (isDevice()) {
+    window.location.href = "shutdown.html";
+}
